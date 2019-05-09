242fe1be985cc1902ca829d1b7af6ac48dfd7739
JoseDJRR <42455421+JoseDJRR@users.noreply.github.com>
2019-05-09 00:58:32 -0500

    act13

--- pinterest/AppDelegate.swift
+++ pinterest/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
-        let initialViewController  = SplashScreen()
+        let initialViewController  = CollectionViewController(collectionViewLayout: Layout())
         let navController = UINavigationController(rootViewController: initialViewController)
         window = UIWindow(frame: UIScreen.main.bounds)
         self.window!.rootViewController = navController
